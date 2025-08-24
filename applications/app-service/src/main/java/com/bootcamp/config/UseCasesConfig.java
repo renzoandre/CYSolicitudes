a924f087e56b416fda91f2069628c8a906369a86
Renzo Condo <renzoandrecm@gmail.com>
2025-08-24 11:41:36 -0500

    Initial commit

---
+++ applications/app-service/src/main/java/com/bootcamp/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package com.bootcamp.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "com.bootcamp.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
